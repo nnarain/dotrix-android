c6da5ffab83652d5a28f32a547e3b1e78edec7cb
Natesh Narain <nnaraindev@gmail.com>
2017-03-17 14:05:42 -0400

    added button to open popmenu, app now full screen mode

--- app/src/main/java/io/github/nnarain/dotrix/ui/Input.java
+++ app/src/main/java/io/github/nnarain/dotrix/ui/Input.java
@@ -3,9 +3,12 @@ package io.github.nnarain.dotrix.ui;
 import android.app.Activity;
 import android.util.Log;
 import android.util.SparseArray;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,16 +23,18 @@ import io.github.nnarain.dotrix.gameboycore.KeyCode;
  * Collect user input and send to GameboyCore
  */
 
-public class Input implements View.OnTouchListener
+public class Input implements View.OnTouchListener, View.OnClickListener
 {
     private static final String TAG = Input.class.getSimpleName();
 
     private final GameboyCore core;
+    private final Activity activity;
     private final SparseArray<KeyCode> keymap;
     private final SparseArray<KeyAction> actionmap;
 
     public Input(Activity activity, GameboyCore core)
     {
+        this.activity = activity;
         this.core = core;
 
         // set key and action maps
@@ -57,22 +62,58 @@ public class Input implements View.OnTouchListener
         activity.findViewById(R.id.bnSelect).setOnTouchListener(this);
         activity.findViewById(R.id.bnStart).setOnTouchListener(this);
 
+        activity.findViewById(R.id.bnMenu).setOnClickListener(this);
     }
 
     @Override
     public boolean onTouch(View v, MotionEvent event)
     {
         final int action = event.getAction();
+        final int id = v.getId();
 
         if(action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_DOWN)
         {
             KeyAction a = this.actionmap.get(action);
             KeyCode code = this.keymap.get(v.getId());
 
-            Log.d(TAG, "Sending input to core: " + a.value() + ", " + code.value());
-            core.input(a, code);
+            if(a != null && code != null)
+            {
+                core.input(a, code);
+            }
         }
 
         return true;
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.bnMenu:
+                showMenu(v);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void showMenu(View v)
+    {
+        PopupMenu popup = new PopupMenu(this.activity, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.main, popup.getMenu());
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener()
+        {
+            @Override
+            public boolean onMenuItemClick(MenuItem item)
+            {
+                Input.this.activity.onOptionsItemSelected(item);
+                return true;
+            }
+        });
+
+        popup.show();
     }
 }
